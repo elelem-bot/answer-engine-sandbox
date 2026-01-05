56ec7abd317cb74b9b1012b1ae6c870bd5200ff6
Tom Salvat <tom.salvat@ko-pylot.com>
2026-01-05 11:05:52 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -6,10 +6,10 @@ import LandingPage from './pages/LandingPage';
 import LandingPage2 from './pages/LandingPage2';
 import NewContent from './pages/NewContent';
 import OptimizeContent from './pages/OptimizeContent';
+import Prompts from './pages/Prompts';
 import Setup from './pages/Setup';
 import Tracking from './pages/Tracking';
 import VisibilityHQ from './pages/VisibilityHQ';
-import Prompts from './pages/Prompts';
 import __Layout from './Layout.jsx';
 
 
@@ -22,10 +22,10 @@ export const PAGES = {
     "LandingPage2": LandingPage2,
     "NewContent": NewContent,
     "OptimizeContent": OptimizeContent,
+    "Prompts": Prompts,
     "Setup": Setup,
     "Tracking": Tracking,
     "VisibilityHQ": VisibilityHQ,
-    "Prompts": Prompts,
 }
 
 export const pagesConfig = {
